0ea73a17760ffa6b87e5ef632302c21f55628727
Alexander <som17son25@gmail.com>
2023-07-17 19:42:02 +0300

    added reposReducer install redux react-redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './app.less'
+import {useDispatch, useSelector} from "react-redux";
+import { setCount} from "../reducers/reposReducer";
 
 const App = () => {
+    const dispatch = useDispatch()
+    //const count = useSelector(state => state.count)
+    const onClick = () => {
+        dispatch(setCount(5))
+    }
     return (
         <div className='app'>
-            React is working
+            <button onClick={() => onClick()}></button>
         </div>
     );
 };
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers, legacy_createStore} from "redux";
+import {applyMiddleware} from "redux";
+import {reposReducer} from "./reposReducer";
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+    repos: reposReducer
+})
+
+
+export const store = legacy_createStore(combineReducers, composeWithDevTools(applyMiddleware(thunk)))
