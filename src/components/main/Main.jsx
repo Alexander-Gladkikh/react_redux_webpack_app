5b8b7c32e3e1369d4b16820273cf967ab4923cbb
Alexander <som17son25@gmail.com>
2023-07-19 15:16:12 +0300

    added search and isFetching

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import './Main.less'
+import React, {useEffect, useState} from 'react';
+import './main.less'
 import {useDispatch, useSelector} from "react-redux";
 import {getRepos} from "../actions/repos";
 import Repo from "./repo/Repo";
@@ -7,17 +7,41 @@ import Repo from "./repo/Repo";
 const Main = () => {
 
     const dispatch = useDispatch();
-    const repos = useSelector(state => state.repos.items)
+    const repos = useSelector(state => state.repos.items);
+    const isFetching = useSelector(state => state.repos.isFetching);
+    const [searchValue, setSearchValue] = useState('')
 
     useEffect(() => {
         dispatch(getRepos())
     }, [])
 
+    const searchHandler = () => {
+        dispatch(getRepos(searchValue))
+    }
+
     return (
         <div>
-            {repos.map(repo =>
-            <Repo repo={repo}/>
-            )}
+            <div className="search">
+                <input
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    type="text" placeholder="Input repo name"
+                    className="search-input"/>
+                <button
+                    className="search-btn"
+                    onClick={() => searchHandler()}
+                >
+                    Search</button>
+            </div>
+            {
+                isFetching === false
+                ?
+                repos.map(repo => <Repo repo={repo}/>)
+                :
+                    <div className="fetching">
+
+                    </div>
+            }
         </div>
     );
 };
